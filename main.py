51e18e50624aec9f33061ad34d3af167a4256653
Utku <utku.aydin@std.yildiz.edu.tr>
2024-08-19 16:53:24 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_experimental.agents.agent_toolkits import create_csv_agent
+
+
+
+load_dotenv()
+
+llm = ChatOpenAI(model = "gpt-3.5-turbo-0125")
+
+agent = create_csv_agent(llm, r"C:\Users\asus\Desktop\Python\Langchain LLM Atıl\RAGProject\cleaned_web_traffic_data (1).csv", verbose=True, allow_dangerous_code=True)
+
+def query_data(query):
+    response = agent.invoke(query)
+    return response
+
+if __name__ == "__main__":
+    user_input = input("> ")
+    query = user_input
+    response = query_data(query)
